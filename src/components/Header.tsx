d07aa14c1aebe5abe0d6132e506642d88b7b4f02
Artem Levchenko <levchencode@gmail.com>
2025-09-06 12:45:13 +0300

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between p-4 lg:p-8">
+            <h1 className="text-3xl font-bold">Pollparlor</h1>
+        </header>
+    );
+}
