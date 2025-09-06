eb68d7fdc4d466238472784a3aa579094e923cf6
Artem Levchenko <levchencode@gmail.com>
2025-09-06 12:45:16 +0300

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex items-center justify-between p-4 lg:p-8">
+            <p className="text-sm">&copy; 2025 Pollparlor</p>
+        </footer>
+    );
+}
