50d5cb901f24de20c1e0e8633f5ab9d65fa96ff3
Artem Levchenko <levchencode@gmail.com>
2025-09-07 19:13:55 +0300

    nav links list

---
+++ src/components/NavLinksList.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useLinksStore } from "@/stores/links-store";
+import Link from "next/link";
+
+export default function NavLinksList() {
+    const links = useLinksStore((state) => state.links);
+
+    return (
+        <ul className="bg-background-accent border-border flex flex-col gap-1 rounded-lg border p-1 md:flex-row md:bg-transparent">
+            {links.map((item) => (
+                <li
+                    key={item.href}
+                    className=""
+                >
+                    <Link
+                        href={"#" + item.href}
+                        className="text-foreground inline-flex w-full items-center gap-3 rounded-md p-2 px-4 dark:hover:bg-zinc-800"
+                    >
+                        {item.icon}
+                        <span>{item.label}</span>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+}
