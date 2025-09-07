ae99d29a2e5fdf97e93edff7b1556e3b0f4c2947
Artem Levchenko <levchencode@gmail.com>
2025-09-07 18:39:31 +0300

    border added to title

--- src/app/(common-ui)/page.tsx
+++ src/app/(common-ui)/page.tsx
@@ -3,11 +3,13 @@ import Poll from "@/components/Poll";
 export default function Home() {
     return (
         <main className="flex flex-1">
-            <section className="relative flex flex-1 flex-col items-center px-4 py-16">
-                <h2 className="absolute top-4 text-center text-xl font-medium">
-                    <span className="sr-only">Poll</span>
-                    Programming languages
-                </h2>
+            <section className="relative flex flex-1 flex-col items-center px-4 py-20">
+                <div className="dark:border-border absolute top-4 rounded-md border p-2">
+                    <h2 className="text-center text-xl font-medium">
+                        <span className="sr-only">Poll</span>
+                        Programming languages
+                    </h2>
+                </div>
 
                 <Poll />
             </section>
