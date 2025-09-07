76358b060508b3d7b011493c247456eb1bd774cd
Artem Levchenko <levchencode@gmail.com>
2025-09-07 19:14:22 +0300

    nav menu for screens >= 768px

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { twMerge } from "tailwind-merge";
+import NavLinksList from "./NavLinksList";
+
+export default function NavMenu({ className }: { className?: string }) {
+    return (
+        <nav className={twMerge(className)}>
+            <NavLinksList />
+        </nav>
+    );
+}
