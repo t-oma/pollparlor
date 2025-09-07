764ce95cbae18e1e5fc4e9348ed6a68b5f69bd2a
Artem Levchenko <levchencode@gmail.com>
2025-09-07 19:13:21 +0300

    h2 wrapper moved up by 8px

--- src/app/(common-ui)/page.tsx
+++ src/app/(common-ui)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <main className="flex flex-1">
             <section className="relative flex flex-1 flex-col items-center px-4 py-20">
-                <div className="dark:border-border absolute top-4 rounded-md border p-2">
+                <div className="dark:border-border absolute top-2 rounded-md border p-2">
                     <h2 className="text-center text-xl font-medium">
                         <span className="sr-only">Poll</span>
                         Programming languages
