4700b8532e9e809f46d64bf4de1f530ceff39dbb
Artem Levchenko <levchencode@gmail.com>
2025-09-09 11:43:25 +0300

    more padding y

--- src/app/(common-ui)/page.tsx
+++ src/app/(common-ui)/page.tsx
@@ -3,7 +3,7 @@ import Poll from "@/components/Poll";
 export default function Home() {
     return (
         <main className="flex flex-1">
-            <section className="relative flex flex-1 flex-col items-center px-4 py-20">
+            <section className="relative flex flex-1 flex-col items-center px-8 py-24">
                 <div className="dark:border-border absolute top-2 rounded-md border p-2">
                     <h2 className="text-center text-xl font-medium">
                         <span className="sr-only">Poll</span>
