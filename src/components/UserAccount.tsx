da3120d8236cb992543ffd15a57fdf9d48f67b51
Artem Levchenko <levchencode@gmail.com>
2025-09-09 11:44:09 +0300

    user account component

---
+++ src/components/UserAccount.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useUserStore } from "@/providers/user-store-provider";
+import Button from "./Button";
+import { LogIn, User } from "lucide-react";
+
+export default function UserAccount() {
+    const { user, login, logout, register } = useUserStore((store) => store);
+
+    return (
+        <div>
+            {user ? (
+                <Button
+                    onClick={() => logout()}
+                    className="md:px-4"
+                >
+                    <User className="h-5 w-5" />
+                    <span className="sr-only font-medium md:not-sr-only">
+                        Account
+                    </span>
+                </Button>
+            ) : (
+                <Button
+                    onClick={() => login("test@test.com", "test")}
+                    className="md:px-4"
+                >
+                    <LogIn className="h-5 w-5" />
+                    <span className="sr-only font-medium md:not-sr-only">
+                        Login
+                    </span>
+                </Button>
+            )}
+        </div>
+    );
+}
