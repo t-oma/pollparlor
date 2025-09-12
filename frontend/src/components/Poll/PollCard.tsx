b3b91268b8f8081c82d997a9cc0d7bca6d87a355
Artem Levchenko <levchencode@gmail.com>
2025-09-13 00:24:46 +0300

    now button rather than Link

--- frontend/src/components/Poll/PollCard.tsx
+++ frontend/src/components/Poll/PollCard.tsx
@@ -1,32 +1,33 @@
 "use client";
 
 import { Poll } from "@/types/poll";
-import Link from "next/link";
+import Button from "../Button";
 
-export default function PollCard({ poll }: { poll: Poll }) {
+export default function PollCard({
+    poll,
+    onClick,
+}: Readonly<{
+    poll: Poll;
+    onClick: () => void;
+}>) {
     return (
-        <Link
-            href={{
-                pathname: `/polls/${poll.id}`,
-            }}
-            className="border-border flex flex-col gap-2 rounded-md border transition-all hover:scale-105 hover:rotate-2"
+        <Button
+            onClick={onClick}
+            className="min-w-[150px] flex-col gap-0 p-4 transition-all hover:scale-105 hover:rotate-2"
         >
-            <div className="flex flex-col px-2 pt-2">
+            <div className="flex flex-1 flex-col items-center justify-center">
                 <h3 className="text-center text-sm font-semibold">
                     {poll.title}
                 </h3>
-                <hr className="border-border my-2" />
-                <p className="text-sm">By: {poll.author.email}</p>
             </div>
-            <div className="border-border flex flex-1 flex-col items-center justify-center border-t p-2">
-                {new Date(poll.createdAt) >= new Date(poll.updatedAt) ? (
-                    <p className="text-sm">{poll.createdAt.toLocaleString()}</p>
-                ) : (
-                    <p className="text-xs">
-                        Update: {poll.updatedAt.toLocaleString()}
-                    </p>
-                )}
-            </div>
-        </Link>
+            <hr className="border-border my-2 w-full" />
+            <p className="text-sm">By: {poll.author.email}</p>
+            <hr className="border-border my-2 w-full" />
+            {new Date(poll.createdAt) >= new Date(poll.updatedAt) ? (
+                <p className="text-sm">{poll.createdAt.toString()}</p>
+            ) : (
+                <p className="text-sm">u: {poll.updatedAt.toString()}</p>
+            )}
+        </Button>
     );
 }
