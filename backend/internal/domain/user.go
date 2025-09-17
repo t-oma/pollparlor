356ddc00468f57f355245a8fa2bdaa932da03daa
Artem Levchenko <levchencode@gmail.com>
2025-09-17 15:52:51 +0300

    bson tags

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -4,14 +4,22 @@ import "time"
 
 // UserVerification represents a user verification status
 type UserVerification struct {
-	Valid bool `json:"valid"`
+	Valid bool `bson:"valid" json:"valid"`
 }
 
 // User represents a user in the system
 type User struct {
-	UUID              string           `json:"uuid"`
-	Email             string           `json:"email"`
-	CreatedAt         time.Time        `json:"createdAt"`
-	PasswordChangedAt time.Time        `json:"passwordChangedAt"`
-	Verification      UserVerification `json:"verification"`
+	UUID              string           `bson:"uuid"              json:"uuid"`
+	Email             string           `bson:"email"             json:"email"`
+	Password          string           `bson:"password"          json:"password"`
+	CreatedAt         time.Time        `bson:"createdAt"         json:"createdAt"`
+	PasswordChangedAt time.Time        `bson:"passwordChangedAt" json:"passwordChangedAt"`
+	Verification      UserVerification `bson:"verification"      json:"verification"`
+}
+
+// UserRepository is a repository for users
+type UserRepository interface {
+	List(limit, skip int64) ([]User, error)
+	GetByID(id string) (*User, error)
+	Create(p User) error
 }
