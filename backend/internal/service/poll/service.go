c95b07cae3279d9a2327df65024bd994c1ac0d87
Artem Levchenko <levchencode@gmail.com>
2025-09-18 12:23:23 +0300

    List & GetByID with author

--- backend/internal/service/poll/service.go
+++ backend/internal/service/poll/service.go
@@ -17,16 +17,36 @@ func New(repo domain.PollRepository) *Service {
 }
 
 // List returns a list of polls
-func (s *Service) List(limit, skip int64) ([]domain.Poll, error) {
+func (s *Service) List(limit, skip int64) ([]domain.PollWithAuthor, error) {
 	return s.repo.List(limit, skip)
 }
 
 // Get returns a poll by ID
-func (s *Service) Get(id bson.ObjectID) (*domain.Poll, error) {
+func (s *Service) Get(id bson.ObjectID) (*domain.PollWithAuthor, error) {
 	return s.repo.GetByID(id)
 }
 
 // Create creates a new poll
 func (s *Service) Create(p domain.Poll) error {
 	return s.repo.Create(p)
+}
+
+// Delete deletes a poll
+func (s *Service) Delete(id bson.ObjectID) error {
+	return s.repo.Delete(id)
+}
+
+// GetItems returns a list of items
+func (s *Service) GetItems(pollID bson.ObjectID) ([]domain.Candidate, error) {
+	return s.repo.GetItems(pollID)
+}
+
+// CreateItems creates a new list of items
+func (s *Service) CreateItems(pollID bson.ObjectID, items []domain.Candidate) error {
+	return s.repo.CreateItems(pollID, items)
+}
+
+// GetPairs returns a list of pairs
+func (s *Service) GetPairs(pollID bson.ObjectID) ([]domain.PollPair, error) {
+	return s.repo.GetPairs(pollID)
 }
